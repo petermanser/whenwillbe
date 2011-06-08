a2441c2169e6f93ba80f7347bb0917ed37cfc6d8
Danilo Bargen <gezuru@gmail.com>
2011-06-08 21:09:14 +0200

    Basic model

--- whenwillbe.py
+++ whenwillbe.py
@@ -1,7 +1,18 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
+from google.appengine.ext import db
 
+### MODELS ###
+
+class Event(db.Model):
+    url = db.StringProperty(required=True)
+    time = db.DateProperty(required=True)
+    name = db.StringProperty(required=True)
+    description = db.StringProperty(multiline=True)
+
+
+### REQUEST HANDLERS ###
 class MainPage(webapp.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
@@ -12,7 +23,11 @@ class Create(webapp.RequestHandler):
     def get(self):
         self.redirect('/', permanent=True)
 
+    def post(self):
+        self.response.out.write('fuck you!\n')
 
+
+### APP HANDLER ###
 
 application = webapp.WSGIApplication([('/', MainPage),
                                       ('/create', Create)
